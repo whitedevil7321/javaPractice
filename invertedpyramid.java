685ddd06b1f4c771d25ff63a0b715b30b8dcf507
Jeevan Aher <83305240+whitedevil7321@users.noreply.github.com>
2023-08-01 08:58:27 +0530

    Create invertedpyramid.java

---
+++ invertedpyramid.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aa;
+
+/**
+ *
+ * @author HP
+ */
+public class Aa {
+
+    public static void main(String[] args) {
+       int n=5;
+      
+       for(int i=n;i>=1;i--){
+           for(int j=i;j>=1;j--){
+               System.out.print("*");
+               
+               
+           }
+           
+           System.out.println();
+       }
+    }
+}
